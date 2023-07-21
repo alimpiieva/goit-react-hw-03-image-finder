20e407cbac647eca64b5b4eeff14fa26aacb3962
Ksenia <aliksukerch@gmail.com>
2023-07-21 21:13:01 +0200

    I hope this is correct

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,17 @@ import ImageGallery from '../ImageGallery/ImageGallery';
 import Searchbar from '../Searchbar/Searchbar';
 import Modal from '../Modal/Modal';
 import fetchImages from '../Api/Api';
+import LoaderComponent from 'components/Loader/Loader';
+import Button from 'components/Button/Button';
 
 class App extends Component {
   state = {
     searchQuery: '',
-    showModal: false,
-    selectedImage: null,
     page: 1,
     images: [],
     isLoading: false,
+    noImagesFound: false,
+    hasMoreImages: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -25,8 +27,6 @@ class App extends Component {
       searchQuery: query,
       page: 1,
       images: [],
-      selectedImage: null,
-      showModal: false,
     });
   };
 
@@ -37,14 +37,18 @@ class App extends Component {
     });
   };
 
+  handleClose = (event) => {
+    const { onClose } = this.props;
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
   handleLoadMore = () => {
     this.setState(
       (prevState) => ({
         page: prevState.page + 1,
       }),
-      () => {
-        this.fetchImages();
-      }
     );
   };
 
@@ -56,12 +60,13 @@ class App extends Component {
       .then((formattedImages) => {
         this.setState((prevState) => ({
           images: [...prevState.images, ...formattedImages],
+          isLoading: false,
+          noImagesFound: formattedImages.length === 0,
+          hasMoreImages: formattedImages.length === 12,
         }));
       })
       .catch((error) => {
         console.error('Error fetching images:', error);
-      })
-      .finally(() => {
         this.setState({ isLoading: false });
       });
   };
@@ -74,22 +79,17 @@ class App extends Component {
   };
 
   render() {
-    const { searchQuery, showModal, selectedImage, images, isLoading } = this.state;
-    const hasMoreImages = images.length > 0 && images.length % 12 === 0;
-    const noImagesFound = !isLoading && images.length === 0 && searchQuery !== '';
+    const { showModal, selectedImage, images, isLoading, noImagesFound, hasMoreImages } = this.state;
 
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSearch} />
-        <ImageGallery
-          images={images}
-          isLoading={isLoading}
-          onImageClick={this.openModal}
-          hasMoreImages={hasMoreImages}
-          noImagesFound={noImagesFound}
-          onLoadMore={this.handleLoadMore}
-        />
+        <ImageGallery images={images} onImageClick={this.openModal} />
         {showModal && <Modal imageData={selectedImage} onClose={this.closeModal} />}
+
+        {isLoading && <LoaderComponent />}
+        {noImagesFound && <p>No images found.</p>}
+        {hasMoreImages && !isLoading && <Button onClick={this.handleLoadMore}>Load More</Button>}
       </div>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Button from '../Button/Button';
+
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-import LoaderComponent from '../Loader/Loader.jsx';
+
 import styles from './ImageGallery.module.css';
 
 class ImageGallery extends Component {
   static propTypes = {
     images: PropTypes.array.isRequired,
-    isLoading: PropTypes.bool.isRequired,
     onImageClick: PropTypes.func.isRequired,
-    onLoadMore: PropTypes.func.isRequired,
-    hasMoreImages: PropTypes.bool.isRequired,
-    noImagesFound: PropTypes.bool.isRequired,
   };
 
   handleImageClick = (image) => {
@@ -21,7 +17,7 @@ class ImageGallery extends Component {
   };
 
   render() {
-    const { images, isLoading, hasMoreImages, noImagesFound, onLoadMore } = this.props;
+    const { images } = this.props;
 
     return (
       <div>
@@ -35,11 +31,6 @@ class ImageGallery extends Component {
             />
           ))}
         </ul>
-        {isLoading && <LoaderComponent />}
-        {noImagesFound && <p>No images found.</p>}
-        {hasMoreImages && !isLoading && (
-          <Button onClick={onLoadMore}>Load More</Button>
-        )}
       </div>
     );
   }
